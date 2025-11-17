f300ba49401f5e9fffde5e2f49ae8fb9f98450a0
venvxiangni <minsbayer@gmail.com>
2025-11-17 16:36:21 +0700

    featur: add post

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -2,26 +2,68 @@ import { text } from "express";
 import {PostCommentsModel,PostClassesModel,UserModel,ClassesModel} from "../models/index.model.js";
 
 
+// Create Post
 export const CreatePost = async (req,res) => {
     try{
 
 
         const userId = req.userId;
         const {classId,title, post} = req.body;
-
-        console.log(req.body)
-
         const createdPost = await PostClassesModel.create({
             user_id: userId,
             class_id: classId,
             title: title,
             text: post
         })
+        
+        const result = await PostClassesModel.findByPk(createdPost.id,{
+            include: [{ model: UserModel, as: 'author', attributes: ['id', 'fullName'] }]
+        })
 
-        return res.status(200).send(createdPost)
+
+        return res.status(200).send(result)
         
 
     }catch(error){
         return res.status(500).send({message: error.message})
+    }
+}
+
+// Get List Post Of A Class
+export const GetPostsClass = async (req,res) => {
+    try{
+        const classId = req.params.classId;
+
+        const posts = await PostClassesModel.findAll({
+            where: {
+                class_id: classId
+            },
+            include:[
+                {
+                    model: UserModel,
+                    as: 'author',
+                    attributes: ['id','fullName']
+                }
+            ],
+            order: [['created_at', 'DESC']]
+        })
+
+        res.status(200).send(posts);
+    }
+    catch(error){
+        return res.status(500).send(error.message)
+    }
+}
+
+export const CreateCommentClass = async (req,res) => {
+    try{
+
+        const userId = req.userId;
+        const {classId,postId,commentId} = req.body;
+
+        
+        
+    }catch(error){
+        return res.status(500).send(error.message)
     }
 }
